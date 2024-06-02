4d0f8f402ea2e7d3549c591d66d06600968a074b
Aiman Azad khan <92166287+aimanazadkhan@users.noreply.github.com>
2024-06-02 13:51:33 +0600

    Add files via upload

---
+++ swadeshfashion/footer.php
@@ -0,0 +1,40 @@
+<footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="footer__about">
+                        <div class="footer__logo">
+                            <a href="index.html"><img src="img/footer logo.png" alt=""  style="max-height: 100px;"></a>
+                        </div>
+                        <p>A Asian tradiational clothing store </p>
+                       
+                    </div>
+                </div>
+                <div class="col-lg-2 offset-lg-3 col-md-3 col-sm-6">
+                    <div class="footer__widget">
+                        <h6>Shopping</h6>
+                        <ul>
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="#about">about us</a></li>
+                            <li><a href="#contact">Contact</a></li>
+                       
+                        </ul>
+                    </div>
+                </div>
+               
+                
+            </div>
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="footer__copyright__text">
+                        
+                        <p>Copyright ©
+                            
+                            All rights reserved
+                        </p>
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
---
+++ swadeshfashion/index.php
@@ -0,0 +1,452 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Swadesh Fashion">
+    <meta name="keywords" content="Swadesh Fashion, saree,punjabi,asian dress,salwar kammez,kids wear">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Swadesh Fashion</title>
+    <meta name="Swadesh Fashion" content="Asian Tradiational clothing brand">
+
+    <link rel="icon" href="img/swadesh.png">
+
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800;900&display=swap"
+        rel="stylesheet">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+
+<style>
+    .logo-img {
+
+        max-height: 100px;
+    }
+</style>
+
+
+
+
+<!-- Offcanvas Menu Begin -->
+<div class="offcanvas-menu-overlay"></div>
+<div class="offcanvas-menu-wrapper">
+
+    <div id="mobile-menu-wrap"></div>
+
+</div>
+<!-- Offcanvas Menu End -->
+
+<!-- Header Section Begin -->
+<?php include "header.php" ?>
+<!-- Header Section End -->
+
+<!-- Hero Section Begin -->
+<section class="hero">
+    <div class="hero__slider owl-carousel">
+        <div class="hero__items set-bg" data-setbg="img/banner/pakistani.webp">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-5 col-lg-7 col-md-8">
+                        <div class="hero__text">
+
+                            <h2>Swadesh Fashion</h2>
+                            <p>A Fashion house where we have Asian tradiational clothing </p>
+                            <!-- <a href="#" class="primary-btn">Shop now <span class="arrow_right"></span></a>
+                                <div class="hero__social">
+                                    <a href="#"><i class="fa fa-facebook"></i></a>
+                                    <a href="#"><i class="fa fa-twitter"></i></a>
+                                    <a href="#"><i class="fa fa-pinterest"></i></a>
+                                    <a href="#"><i class="fa fa-instagram"></i></a>
+                                </div> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+<!-- Hero Section End -->
+
+<!-- Banner Section Begin -->
+<section class="banner spad">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-7 offset-lg-4 ">
+
+
+
+                <div class="banner__item ">
+                    <div class="banner__item__pic ">
+                        <img src="img/banner/saree.jpg" alt="" style="width: 600px; height: 600px;">
+                    </div>
+                    <div class="banner__item__text">
+                        <h2>Women's Wear</h2>
+
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-lg-5">
+                <div class="banner__item banner__item--middle">
+                    <div class="banner__item__pic">
+                        <img src="img/banner/punjabi.png" alt="">
+                    </div>
+                    <div class="banner__item__text">
+                        <h2>Men's Wear</h2>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-7 ">
+                <div class="banner__item banner__item--last">
+                    <div class="banner__item__pic">
+                        <img src="img/banner/kidss.jpeg" alt="" style="width: 600px;">
+                    </div>
+                    <div class="banner__item__text">
+                        <h2>Kids wear</h2>
+
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</section>
+
+
+<section class="about spad" id="about">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="about__pic">
+                    <img src="img/about/about-us.jpg" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-6">
+                <div class="about__item">
+                    <h4>Who We Are ?</h4>
+                    <p>Welcome to Swadesh Fashion, your ultimate destination for exquisite Asian traditional
+                        clothing. We take pride in offering a diverse collection that celebrates the rich cultural
+                        heritage and timeless elegance of traditional attire.</p>
+
+
+                        <h4 style="margin-top: 7px;">Our Location</h4>
+
+                        <p>Monal Market , Unit 11, 692-696 Stratford Road, Sparkhill, Birmingham, B11 4AT</p>
+
+
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-6">
+                <div class="about__item">
+                    <h4>What We sell?</h4>
+                    <li><strong>Sarees:</strong> From luxurious silks to elegant cottons, our sarees are perfect for
+                        any occasion.</li>
+                    <li><strong>Salwar Suits:</strong> Comfortable yet stylish, our salwar suits come in a variety
+                        of designs and fabrics.</li>
+                    <li><strong>Maxi Dresses:</strong> Flowing, graceful, and perfect for modern wear with a
+                        traditional touch.</li>
+                    <li><strong>Blouses:</strong> Beautifully crafted to pair perfectly with our sarees.</li>
+                    <li><strong>Abayas:</strong> Modest yet fashionable, our abayas blend tradition with
+                        contemporary trends.</li>
+                    <li><strong>Jewellery:</strong> Enhance your outfit with our stunning range of traditional
+                        jewellery.</li>
+                    <li><strong>Menswear:</strong> Elegant and sophisticated traditional attire for men.</li>
+                    <li><strong>Kidswear:</strong> Adorable and comfortable traditional clothing for children<br>
+                    <strong>And many more.....</strong>
+                </li>
+                    
+
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-6">
+                <div class="about__item">
+                    <h4>Why Choose Us</h4>
+                    <li><strong>Quality:</strong> We use only the finest materials and craftsmanship in every piece.
+                    </li>
+                    <li><strong>Variety:</strong> Our extensive range ensures that you find the perfect outfit for any
+                        occasion.</li>
+                    <li><strong>Customer Service:</strong> We are dedicated to providing excellent customer service and
+                        ensuring a delightful shopping experience.</li>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="contact spad" id="contact">
+    <div class="map">
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2426.4362358079445!2d-1.8990833841930233!3d52.48624367980839!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4870bc98b4146e13%3A0x9a0e5650c763b663!2sBirmingham%2C%20UK!5e0!3m2!1sen!2sbd!4v1597926938024!5m2!1sen!2sbd"
+            height="500" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    </div>
+    <div class="container " style="margin-top:50px;">
+        <div class="row">
+            <div class="col-lg-6 col-md-6">
+                <div class="contact__text">
+                    <div class="section-title">
+                        <span>Information</span>
+                        <h2>Contact Us</h2>
+                        <p>As you might expect of a company that began as a high-end interiors contractor, we pay
+                            strict attention.</p>
+                    </div>
+                    <ul>
+                        <li>
+                            <h4>UNITED KINGDOM</h4>
+                            <p>swadeshfashion123@gmail.com</p>
+                        </li>
+                        <li>
+
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+<!-- Banner Section End -->
+
+<!-- Product Section Begin -->
+<!-- <section class="product spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <ul class="filter__controls">
+                        <li class="active" data-filter="*">Best Sellers</li>
+                        <li data-filter=".new-arrivals">New Arrivals</li>
+                        <li data-filter=".hot-sales">Hot Sales</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row product__filter">
+                <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix new-arrivals">
+                    <div class="product__item">
+                        <div class="product__item__pic set-bg" data-setbg="img/banner/salawar suit.webp">
+                            <span class="label">New</span>
+                           
+                        </div>
+                        <div class="product__item__text">
+                            <h6>salwar suit</h6>
+                          
+                           
+                           
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix hot-sales">
+                    <div class="product__item">
+                        <div class="product__item__pic set-bg" data-setbg="img/product/product-2.jpg">
+                           
+                        </div>
+                        <div class="product__item__text">
+                            <h6>jewellery</h6>
+                          
+                           
+                         
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix new-arrivals">
+                    <div class="product__item sale">
+                        <div class="product__item__pic set-bg" data-setbg="img/product/product-3.jpg">
+                            <span class="label">Sale</span>
+                           
+                        </div>
+                        <div class="product__item__text">
+                            <h6>shoes</h6>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix hot-sales">
+                    <div class="product__item">
+                        <div class="product__item__pic set-bg" data-setbg="img/product/product-4.jpg">
+                           
+                        </div>
+                        <div class="product__item__text">
+                            <h6>blouse</h6>
+                          
+                        
+                           
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix new-arrivals">
+                    <div class="product__item">
+                        <div class="product__item__pic set-bg" data-setbg="img/product/product-5.jpg">
+                           
+                        </div>
+                        <div class="product__item__text">
+                            <h6>abaya</h6>
+                          
+                           
+                          
+                           
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix hot-sales">
+                    <div class="product__item sale">
+                        <div class="product__item__pic set-bg" data-setbg="img/product/product-6.jpg">
+                            <span class="label">Sale</span>
+                           
+                        </div>
+                        <div class="product__item__text">
+                            <h6>kids wear</h6>
+                          
+                          
+                         
+                           
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix new-arrivals">
+                    <div class="product__item">
+                        <div class="product__item__pic set-bg" data-setbg="img/product/product-7.jpg">
+                           
+                        </div>
+                        <div class="product__item__text">
+                            <h6>T-shirt Contrast Pocket</h6>
+                          
+                           
+                           
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix hot-sales">
+                    <div class="product__item">
+                        <div class="product__item__pic set-bg" data-setbg="img/product/product-8.jpg">
+                           
+                        </div>
+                        <div class="product__item__text">
+                            <h6>Basic Flowing Scarf</h6>
+                          
+                           
+                          
+                           
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> -->
+<!-- Product Section End -->
+
+<!-- Categories Section Begin -->
+<!-- <section class="categories spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="categories__text">
+                        <h2>Clothings Hot <br /> <span>Shoe Collection</span> <br /> Accessories</h2>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="categories__hot__deal">
+                        <img src="img/product-sale.png" alt="">
+                        <div class="hot__deal__sticker">
+                            <span>Sale Of</span>
+                            <h5>$29.99</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 offset-lg-1">
+                    <div class="categories__deal__countdown">
+                        <span>Deal Of The Week</span>
+                        <h2>Multi-pocket Chest Bag Black</h2>
+                        <div class="categories__deal__countdown__timer" id="countdown">
+                            <div class="cd-item">
+                                <span>3</span>
+                                <p>Days</p>
+                            </div>
+                            <div class="cd-item">
+                                <span>1</span>
+                                <p>Hours</p>
+                            </div>
+                            <div class="cd-item">
+                                <span>50</span>
+                                <p>Minutes</p>
+                            </div>
+                            <div class="cd-item">
+                                <span>18</span>
+                                <p>Seconds</p>
+                            </div>
+                        </div>
+                        <a href="#" class="primary-btn">Shop now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> -->
+<!-- Categories Section End -->
+
+<!-- Instagram Section Begin -->
+<!-- <section class="instagram spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="instagram__pic">
+                        <div class="instagram__pic__item set-bg" data-setbg="img/instagram/instagram-1.jpg"></div>
+                        <div class="instagram__pic__item set-bg" data-setbg="img/instagram/instagram-2.jpg"></div>
+                        <div class="instagram__pic__item set-bg" data-setbg="img/instagram/instagram-3.jpg"></div>
+                        <div class="instagram__pic__item set-bg" data-setbg="img/instagram/instagram-4.jpg"></div>
+                        <div class="instagram__pic__item set-bg" data-setbg="img/instagram/instagram-5.jpg"></div>
+                        <div class="instagram__pic__item set-bg" data-setbg="img/instagram/instagram-6.jpg"></div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="instagram__text">
+                        <h3>Instagram</h3>
+                        <p>Welcome to Swadesh Fashion your ultimate destination for exquisite Asian traditional clothing. We take pride in offering a diverse collection that celebrates the rich cultural heritage and timeless elegance of traditional attire</p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> -->
+<!-- Instagram Section End -->
+
+<!-- Latest Blog Section Begin -->
+
+<!-- Latest Blog Section End -->
+
+<!-- Footer Section Begin -->
+<?php include "footer.php" ?>
+<!-- Footer Section End -->
+
+<!-- Search Begin -->
+
+<!-- Search End -->
+
+<!-- Js Plugins -->
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.nice-select.min.js"></script>
+<script src="js/jquery.nicescroll.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/jquery.countdown.min.js"></script>
+<script src="js/jquery.slicknav.js"></script>
+<script src="js/mixitup.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+
+</html>
